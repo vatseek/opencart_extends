2f058c4f1766ee270e50c71382009f28d111bd5c
vatseek <vatseek13@rambler.ru>
2014-07-17 21:35:17 +0300

    init

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://test.co/admin/');
+define('HTTP_CATALOG', 'http://test.co/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://test.co/admin/');
+define('HTTPS_CATALOG', 'http://test.co/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/test.co/admin/');
+define('DIR_SYSTEM', '/var/www/test.co/system/');
+define('DIR_DATABASE', '/var/www/test.co/system/database/');
+define('DIR_LANGUAGE', '/var/www/test.co/admin/language/');
+define('DIR_TEMPLATE', '/var/www/test.co/admin/view/template/');
+define('DIR_CONFIG', '/var/www/test.co/system/config/');
+define('DIR_IMAGE', '/var/www/test.co/image/');
+define('DIR_CACHE', '/var/www/test.co/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/test.co/download/');
+define('DIR_LOGS', '/var/www/test.co/system/logs/');
+define('DIR_CATALOG', '/var/www/test.co/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '72546831');
+define('DB_DATABASE', 'test');
+define('DB_PREFIX', '');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://test.co/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://test.co/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/test.co/catalog/');
+define('DIR_SYSTEM', '/var/www/test.co/system/');
+define('DIR_DATABASE', '/var/www/test.co/system/database/');
+define('DIR_LANGUAGE', '/var/www/test.co/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/test.co/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/test.co/system/config/');
+define('DIR_IMAGE', '/var/www/test.co/image/');
+define('DIR_CACHE', '/var/www/test.co/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/test.co/download/');
+define('DIR_LOGS', '/var/www/test.co/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '72546831');
+define('DB_DATABASE', 'test');
+define('DB_PREFIX', '');
+?>
